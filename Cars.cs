a87f4a2e6c68cf54ed84595d15d126e3f332d747
Beverly Potts <pottsbeverly@gmail.com>
2020-07-13 12:57:22 -0400

    Add initial files
    co-authored-by: Sara Kane <kane.sarak@gmail.com>
    Co-authored-by:  Joseph Pearce <joseph.pearce05@gmail.com>

---
+++ Cars.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+public class Car
+{
+  public string MakeModel;
+  public int Price;
+  public int Miles;
+}
+
+public class Program
+{
+  public static void Main()
+  {
+
+  }
+}
