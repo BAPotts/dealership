dfd671023c21868606bda9bb3971b90de00796a3
Beverly Potts <pottsbeverly@gmail.com>
2020-07-13 16:44:02 -0400

    Write public  properties to access private fields
    co-authored-by: Sara Kane <kane.sarak@gmail.com>
    Co-authored-by:  Joseph Pearce <joseph.pearce05@gmail.com>

--- Program.cs
+++ Program.cs
@@ -14,29 +14,10 @@ namespace Dealership
       Car yugo = new Car("1980 Yugo Koral", 700, 56000);
       Car ford = new Car("1988 Ford Country Squire", 1400, 239001);
       Car amc = new Car("1976 AMC Pacer", 400, 198000);
-      // Car volkswagen = new Car();
-      // volkswagen.MakeModel = "1974 Volkswagen Thing";
-      // volkswagen.Price = 1100;
-      // volkswagen.Miles = 368792;
-
-      // Car yugo = new Car();
-      // yugo.MakeModel = "1980 Yugo Koral";
-      // yugo.Price = 700;
-      // yugo.Miles = 56000;
-
-      // Car ford = new Car();
-      // ford.MakeModel = "1988 Ford Country Squire";
-      // ford.Price = 1400;
-      // ford.Miles = 239001;
-
-      // Car amc = new Car();
-      // amc.MakeModel = "1976 AMC Pacer";
-      // amc.Price = 400;
-      // amc.Miles = 198000;
-
+      
       List<Car> cars = new List<Car>() { volkswagen, yugo, ford, amc };
 
-      yugo.SetPrice(300);
+      yugo.Price =300;
 
       Console.WriteLine("Enter maximum price: ");
       string stringMaxPrice = Console.ReadLine();
@@ -55,9 +36,9 @@ namespace Dealership
       foreach(Car automobile in carsMatchingSearch)
       {
         Console.WriteLine("----------------------");
-        Console.WriteLine(automobile.GetMakeModel());
-        Console.WriteLine(automobile.GetMiles() + " miles");
-        Console.WriteLine("$" + automobile.GetPrice());
+        Console.WriteLine(automobile.MakeModel);
+        Console.WriteLine(automobile.Miles + " miles");
+        Console.WriteLine("$" + automobile.Price);
       }
     }
   }
